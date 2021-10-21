55b18f8211434d499d9d260690d19c25a23c0047
Saket Pandey <55249292+dexter-11@users.noreply.github.com>
2021-10-21 15:23:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<?php
+echo '<title>Sub Domain Takeover Possible!</title>';
+?>
+</head>
+<body>
+<?php
+       echo '<p>HAHAHAH.. Gotchu!</p>';
+?>
+<p>And this content is static HTML</p>
+</body>
+</html>
